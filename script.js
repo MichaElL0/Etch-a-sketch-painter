48e7dcf66e47ff238ab928a88b32de7afd00dd01
MichaEIL0 <dkujoo@gmail.com>
2024-03-30 16:24:11 +0100

    Add limit to prompted number to prevent from to high numbers being inputted

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("#grid-size-button");
-let gridSize = null;
+let gridSize = 16;
 
 for (let i = 1; i <= 16; i++) {
     for (let x = 1; x <= 16; x++) {
@@ -10,16 +10,30 @@ for (let i = 1; i <= 16; i++) {
     }
 }
 
+button.addEventListener("click", e => {
+    gridSize = prompt("Input a number of squares per side: ", 16);
+    if(gridSize > 100) {
+        alert("Number can't be larger than 100!");
+        gridSize = 16;
+    }
+
+    container.textContent = "";
+
+    for (let i = 1; i <= gridSize; i++) {
+        for (let x = 1; x <= gridSize; x++) {
+            const div = document.createElement("div");
+            div.className = `cell num-${i}-${x}`;
+            container.appendChild(div);
+        }
+    }
+});
+
+
+
 const divs = document.querySelectorAll(".container div");
 
 divs.forEach(item => {
     item.addEventListener("mouseover", e => {
         e.target.style.backgroundColor = "blue";
     })
-});
-
-
-
-button.addEventListener("click", e => {
-    gridSize = prompt("Input a number of squares per side: ");
 });
