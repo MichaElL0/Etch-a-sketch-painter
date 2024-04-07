aae26f9657c760963baa5be21bb13c840f6a575c
MichaEIL0 <dkujoo@gmail.com>
2024-04-07 16:59:11 +0200

    Opacity button work as intended. Change styles for buttons when clicked etc.

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const rainbowButton = document.querySelector("#rainbow-button");
 const opacityButton = document.querySelector("#opacity-button");
 let gridSize = 16;
 let isRainbow = true;
+let isOpacity = true;
 
 drawGrid(gridSize);
 
+//Draw grid based on inputted size
 function drawGrid(size) {
     for (let i = 1; i <= size; i++) {
         for (let x = 1; x <= size; x++) {
@@ -24,15 +26,16 @@ function drawGrid(size) {
         let opacity = 100;
         item.addEventListener("mouseover", e => {
             if(isRainbow) {
-                e.target.style.backgroundColor = `hsl(${Math.floor(Math.random() * 255)}, 100%, ${opacity-=10}%)`;
+                e.target.style.backgroundColor = `hsl(${Math.floor(Math.random() * 255)}, 100%, ${isOpacity ? opacity-=10 : opacity = 50}%)`;
             }
             else {
-                e.target.style.backgroundColor = "blue";
+                e.target.style.backgroundColor = `hsl(200, 100%, ${isOpacity ? opacity-=10 : opacity = 50}%)`;
             }
         })
     });
 }
 
+//Check if number of new grid is between limits and depending on that draw new grid
 gridSizeButton.addEventListener("click", e => {
     gridSize = prompt("Input a number of squares per side: ", 16);
     if(gridSize > 100 || gridSize < 2) {
@@ -45,6 +48,7 @@ gridSizeButton.addEventListener("click", e => {
     drawGrid(gridSize);
 });
 
+//Change every cell to default color on click
 resetButton.addEventListener("click", e => {
     const divs = document.querySelectorAll(".container div");
     
@@ -55,6 +59,7 @@ resetButton.addEventListener("click", e => {
 
 let rainbowCounter = 1;
 
+//Check if button is clicked or not and based on that add logic and style
 rainbowButton.addEventListener("click", e => {
     rainbowCounter++;
 
@@ -70,22 +75,26 @@ rainbowButton.addEventListener("click", e => {
     }
 });
 
+//Change color of rainbow button based on X and Y position of mouse
 window.addEventListener("mousemove", e => {
     rainbowButton.style.color = `rgb(${e.offsetX}, ${e.offsetY}, 220)`;
 });
 
 let opacityCounter = 1;
 
+//Check if button is clicked or not and based on that add logic and style
 opacityButton.addEventListener("click", e => {
     opacityCounter++;
 
     if(opacityCounter % 2 == 0) {
         //is unclicked
         opacityButton.classList.remove("button-clicked");
+        isOpacity = false;
     }
     else if(opacityCounter % 2 != 0) {
         //is clicked
         opacityButton.classList.add("button-clicked");
+        isOpacity = true;
     }
 });
 
